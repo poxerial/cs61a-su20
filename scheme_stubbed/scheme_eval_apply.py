29e7ff4d9e9b31f2c54df3e549383cb436f35e5a
poxerial <poxerial@gmail.com>
2022-08-05 22:32:27 +0800

    -

--- scheme_stubbed/scheme_eval_apply.py
+++ scheme_stubbed/scheme_eval_apply.py
@@ -1,3 +1,4 @@
+from threading import local
 import scheme_forms
 from ucb import main, trace
 from scheme_builtins import *
@@ -29,49 +30,50 @@ def other_builtin(name):
     def add(py_func):
         other_builtins[name] = py_func
         return py_func
+
     return add
 
 
-@other_builtin('lambda')
+@other_builtin("lambda")
 def scheme_lambda(expr_rest, env: Frame):
     formals = expr_rest.first
     body = expr_rest.rest
     if body == nil:
-        raise SchemeError('Invalid lambda expressing!')
+        raise SchemeError("Invalid lambda expressing!")
     return LambdaProcedure(formals, body, env)
 
 
-@other_builtin('define')
+@other_builtin("define")
 def scheme_define(expr_rest, env: Frame):
     name = expr_rest.first
     if type(name) == Pair:
         formals = name.rest
         name = name.first
         if type(name) != str:
-            raise SchemeError('Invalid defining!')
+            raise SchemeError("Invalid defining!")
         body = expr_rest.rest
         func = LambdaProcedure(formals, body, env)
         env.define(name, func)
         func.env.define(name, func)
         return name
     if type(name) != str or name in other_builtins or builtin_find(name):
-        raise SchemeError('Invalide define name ' + str(name))
+        raise SchemeError("Invalide define name " + str(name))
     value = scheme_eval(expr_rest.rest.first, env)
     env.define(name, value)
     return name
 
 
-@other_builtin('eval')
+@other_builtin("eval")
 def eval_in_scheme(expr_rest, env: Frame):
     return scheme_eval(scheme_eval(expr_rest.first, env), env)
 
 
-@other_builtin('quote')
+@other_builtin("quote")
 def scheme_quote(expr_rest, env: Frame):
     return expr_rest.first
 
 
-@other_builtin('begin')
+@other_builtin("begin")
 def scheme_begin(expr_rest, env: Frame):
     rt = None
     while expr_rest != nil:
@@ -80,7 +82,7 @@ def scheme_begin(expr_rest, env: Frame):
     return rt
 
 
-@other_builtin('and')
+@other_builtin("and")
 def scheme_and(expr_rest, env):
     val = True
     while expr_rest != nil:
@@ -91,7 +93,7 @@ def scheme_and(expr_rest, env):
     return val
 
 
-@other_builtin('or')
+@other_builtin("or")
 def sheme_or(expr_rest, env):
     val = False
     while expr_rest != nil:
@@ -102,14 +104,14 @@ def sheme_or(expr_rest, env):
     return val
 
 
-@other_builtin('if')
+@other_builtin("if")
 def scheme_if(expr_rest, env: Frame):
     if scheme_true(scheme_eval(expr_rest.first, env)):
         return scheme_eval(expr_rest.rest.first, env)
     return scheme_eval(expr_rest.rest.rest.first, env)
 
 
-@other_builtin('cond')
+@other_builtin("cond")
 def scheme_cond(expr_rest, env):
     while expr_rest != nil:
         temp = expr_rest.first
@@ -125,7 +127,7 @@ def scheme_cond(expr_rest, env):
         expr_rest = expr_rest.rest
 
 
-@other_builtin('let')
+@other_builtin("let")
 def scheme_let(expr_rest, env):
     let_frame = Frame(env)
     define_list = expr_rest.first
@@ -133,9 +135,9 @@ def scheme_let(expr_rest, env):
         pair = define_list.first
         let_frame.define(pair.first, scheme_eval(pair.rest.first, env))
         if pair.rest.rest != nil:
-            raise SchemeError('There should be one value to asign in let form!')
+            raise SchemeError("There should be one value to asign in let form!")
         define_list = define_list.rest
-    
+
     to_eval = expr_rest.rest
     while to_eval != nil:
         val = scheme_eval(to_eval.first, let_frame)
@@ -143,14 +145,14 @@ def scheme_let(expr_rest, env):
     return val
 
 
-@other_builtin('mu')
+@other_builtin("mu")
 def scheme_mu(expr_rest, env):
     formals = expr_rest.first
     body = expr_rest.rest
     if body == nil:
-        raise SchemeError('Invalid mu expressing!')
+        raise SchemeError("Invalid mu expressing!")
     return MuProcedure(formals, body)
-    
+
 
 def scheme_eval(expr, env, _=None):  # Optional third argument is ignored
     """Evaluate Scheme expression EXPR in Frame ENV.
@@ -164,34 +166,36 @@ def scheme_eval(expr, env, _=None):  # Optional third argument is ignored
     # BEGIN Problem 1/2
     if type(expr) != Pair:
         if type(expr) == str:
-            if expr == 'else':
+            if expr == "else":
                 return True
             val = env.find(expr)
             if val != None:
                 return val
             if builtin_find(expr):
-                return '#'+expr
+                return "#" + expr
             else:
-                raise SchemeError('Undefined name: ' + str(expr))
+                raise SchemeError("Undefined name: " + str(expr))
         return expr
 
     operator_name = expr.first
     if type(operator_name) != str:
         if type(operator_name) == Pair:
-            prcd = scheme_eval(operator_name, env)
-            return complete_apply(prcd, expr.rest, env)
+            prcd = scheme_eval(operator_name, env, _=_)
+            return scheme_apply(prcd, expr.rest, env)
         else:
-            raise SchemeError('No valid operator!')
+            raise SchemeError("No valid operator!")
 
     if operator_name in other_builtins:
         return other_builtins[operator_name](expr.rest, env)
 
     prcd = env.find(operator_name)
+    if _ and prcd == _:
+        return Unevaluated(expr, env=env)
     if prcd:
         if isinstance(prcd, Procedure):
-            return complete_apply(prcd, expr.rest, env)
+            return scheme_apply(prcd, expr.rest, env)
 
-    raise SchemeError('Undefined operator ' + str(operator_name) + '!')
+    raise SchemeError("Undefined operator " + str(operator_name) + "!")
     # END Problem 1/2
 
 
@@ -199,17 +203,27 @@ def scheme_apply(procedure, args, env):
     """Apply Scheme PROCEDURE to argument values ARGS (a Scheme list) in
     Frame ENV, the current environment."""
     # BEGIN Problem 1/2
-    py_args = []
-    while args != nil:
-        py_args.append(scheme_eval(args.first, env))
-        args = args.rest
-    try:
-        if procedure.expect_env:
-            return procedure.py_func(*py_args, env)
-        else:
-            return procedure.py_func(*py_args)
-    except TypeError:
-        raise (SchemeError(TypeError))
+    if type(procedure) == BuiltinProcedure:
+        py_args = []
+        while args != nil:
+            py_args.append(scheme_eval(args.first, env))
+            args = args.rest
+        try:
+            if procedure.expect_env:
+                return procedure.py_func(*py_args, env)
+            else:
+                return procedure.py_func(*py_args)
+        except TypeError:
+            raise SchemeError(TypeError)
+    elif type(procedure) == LambdaProcedure:
+        func_frame = Frame(procedure.env)
+        define_args(procedure, args, func_frame, env)
+        temp = non_tail_eval(procedure, func_frame)
+        return tail_eval(procedure, temp.first, func_frame)
+    elif type(procedure) == MuProcedure:
+        return scheme_apply(
+            LambdaProcedure(procedure.formals, procedure.body, env), args, env
+        )
     # END Problem 1/2
 
 
@@ -219,6 +233,41 @@ def scheme_apply(procedure, args, env):
 
 # Make classes/functions for creating tail recursive programs here!
 # BEGIN Problem EC 1
+def define_args(procedure, args, func_frame, env):
+    """Define args in `func_frame`."""
+    formals = procedure.formals
+    while args != nil:
+        if formals == nil:
+            raise SchemeError("Invalid args parsed to procedure!")
+        func_frame.define(formals.first, scheme_eval(args.first, env))
+        args = args.rest
+        formals = formals.rest
+    if formals != nil:
+        raise SchemeError("Invalid args parsed to procedure!")
+
+
+def non_tail_eval(procedure, func_frame):
+    """Evaluate all the exprs in `procedure` except the tail."""
+    temp = procedure.body
+    while temp.rest != nil:
+        val = scheme_eval(temp.first, func_frame)
+        temp = temp.rest
+    return temp
+
+
+def tail_eval(procedure, tail, func_frame: Frame):
+    """Implement tail recursion without create new frame."""
+    return scheme_eval(tail, func_frame, _=procedure)
+
+
+class Unevaluated:
+    """Unevaluated expresions in `tail_eval`."""
+
+    def __init__(self, expr, env):
+        self.expr = expr
+        self.env = env
+
+
 # END Problem EC 1
 
 
@@ -228,28 +277,4 @@ def complete_apply(procedure, args, env):
     if you attempt the extra credit."""
     validate_procedure(procedure)
     # BEGIN
-    if type(procedure) == BuiltinProcedure:
-        return scheme_apply(procedure, args, env)
-    elif type(procedure) == LambdaProcedure:
-        func_frame = Frame(procedure.env)
-        formals = procedure.formals
-
-        
-
-        while args != nil:
-            if formals == nil:
-                raise SchemeError('Invalid args parsed to procedure!')
-            func_frame.define(
-                formals.first, scheme_eval(args.first, env))
-            args = args.rest
-            formals = formals.rest
-        if formals != nil:
-            raise SchemeError('Invalid args parsed to procedure!')
-        val = scheme_eval(procedure.body.first, func_frame)
-        temp = procedure.body.rest
-        while temp != nil:
-            val = scheme_eval(temp.first, func_frame)
-            temp = temp.rest
-        return val
     # END
-
--- scheme_stubbed/test.py
+++ scheme_stubbed/test.py
@@ -1,8 +1,13 @@
 from scheme import *
 
-expr = read_line('(define (f a) (if (< a 0) a (f (- a 1))))')
-f = create_global_frame()
-scheme_eval(expr, f)
+def all_eval(exprs):
+    env = create_global_frame()
+    for expr in exprs:
+        print(scheme_eval(read_line(expr), env))
 
+all_eval(['(+ 1 1)'])
+#all_eval(['(define square (lambda (x) (* x x)))', '(define (sum-of-squares x y) (+ (square x) (square y)))', '(sum-of-squares 3 4)'])
+"""
 print(scheme_eval(read_line('(f 1000)'), f))
 print(type(scheme_eval(read_line('(f 1000)'), f)))
+"""
