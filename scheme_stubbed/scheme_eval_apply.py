94d3f1f68b9ed218facc982054a335bc8a934fb4
surface <poxerial@gmail.com>
2022-08-06 17:48:09 +0800

    tail recursion

--- scheme_stubbed/scheme_eval_apply.py
+++ scheme_stubbed/scheme_eval_apply.py
@@ -1,12 +1,29 @@
-from threading import local
-import scheme_forms
-from ucb import main, trace
+from reprlib import recursive_repr
+from scheme_classes import *
+from scheme_forms import *
+from scheme_eval_apply import *
 from scheme_builtins import *
-from scheme_utils import *
-from pair import *
-import sys
+from scheme_reader import *
+from ucb import main, trace
+from copy import deepcopy
 
-sys.setrecursionlimit(5000)
+def add_builtins(frame, funcs_and_names):
+    """Enter bindings in FUNCS_AND_NAMES into FRAME, an environment frame,
+    as built-in procedures. Each item in FUNCS_AND_NAMES has the form
+    (NAME, PYTHON-FUNCTION, INTERNAL-NAME)."""
+    for name, py_func, proc_name, expect_env in funcs_and_names:
+        frame.define(name, BuiltinProcedure(py_func, name=proc_name, expect_env=expect_env))
+
+def create_global_frame():
+    """Initialize and return a single-frame environment with built-in names."""
+    env = Frame(None)
+    env.define('eval',
+               BuiltinProcedure(scheme_eval, True, 'eval'))
+    env.define('apply',
+               BuiltinProcedure(complete_apply, True, 'apply'))
+    env.define('undefined', None)
+    add_builtins(env, BUILTINS)
+    return env
 
 ##############
 # Eval/Apply #
@@ -30,12 +47,11 @@ def other_builtin(name):
     def add(py_func):
         other_builtins[name] = py_func
         return py_func
-
     return add
 
 
 @other_builtin("lambda")
-def scheme_lambda(expr_rest, env: Frame):
+def scheme_lambda(expr_rest, env: Frame, _=None):
     formals = expr_rest.first
     body = expr_rest.rest
     if body == nil:
@@ -44,7 +60,7 @@ def scheme_lambda(expr_rest, env: Frame):
 
 
 @other_builtin("define")
-def scheme_define(expr_rest, env: Frame):
+def scheme_define(expr_rest, env: Frame, _=None):
     name = expr_rest.first
     if type(name) == Pair:
         formals = name.rest
@@ -64,29 +80,32 @@ def scheme_define(expr_rest, env: Frame):
 
 
 @other_builtin("eval")
-def eval_in_scheme(expr_rest, env: Frame):
+def eval_in_scheme(expr_rest, env: Frame, _=None):
     return scheme_eval(scheme_eval(expr_rest.first, env), env)
 
 
 @other_builtin("quote")
-def scheme_quote(expr_rest, env: Frame):
+def scheme_quote(expr_rest, env: Frame, _=None):
     return expr_rest.first
 
 
 @other_builtin("begin")
-def scheme_begin(expr_rest, env: Frame):
+def scheme_begin(expr_rest, env: Frame, _=None):
     rt = None
     while expr_rest != nil:
-        rt = scheme_eval(expr_rest.first, env)
+        rt = scheme_eval(expr_rest.first, env, _=_)
         expr_rest = expr_rest.rest
     return rt
 
 
 @other_builtin("and")
-def scheme_and(expr_rest, env):
+def scheme_and(expr_rest, env, _=None):
     val = True
     while expr_rest != nil:
-        val = scheme_eval(expr_rest.first, env)
+        if expr_rest.rest == nil:
+            val = scheme_eval(expr_rest.first, env, _=_)
+        else:
+            val = scheme_eval(expr_rest.first, env)
         if not scheme_true(val):
             return val
         expr_rest = expr_rest.rest
@@ -94,10 +113,13 @@ def scheme_and(expr_rest, env):
 
 
 @other_builtin("or")
-def sheme_or(expr_rest, env):
+def sheme_or(expr_rest, env, _=None):
     val = False
     while expr_rest != nil:
-        val = scheme_eval(expr_rest.first, env)
+        if expr_rest.rest == nil:
+            val = scheme_eval(expr_rest.first, env, _=_)
+        else:
+            val = scheme_eval(expr_rest.first, env)
         if scheme_true(val):
             return val
         expr_rest = expr_rest.rest
@@ -105,48 +127,52 @@ def sheme_or(expr_rest, env):
 
 
 @other_builtin("if")
-def scheme_if(expr_rest, env: Frame):
+def scheme_if(expr_rest, env: Frame, _=None):
     if scheme_true(scheme_eval(expr_rest.first, env)):
-        return scheme_eval(expr_rest.rest.first, env)
-    return scheme_eval(expr_rest.rest.rest.first, env)
+        return scheme_eval(expr_rest.rest.first, env, _=_)
+    return scheme_eval(expr_rest.rest.rest.first, env, _=_)
 
 
 @other_builtin("cond")
-def scheme_cond(expr_rest, env):
+def scheme_cond(expr_rest, env, _=None):
     while expr_rest != nil:
         temp = expr_rest.first
-        val = scheme_eval(temp.first, env)
+        val = scheme_eval(temp.first, env, _=_)
         if scheme_true(val):
             if temp.rest == nil:
                 return val
             todo = temp.rest
             while todo != nil:
-                val = scheme_eval(todo.first, env)
+                val = scheme_eval(todo.first, env, _=_)
                 todo = todo.rest
             return val
         expr_rest = expr_rest.rest
 
 
 @other_builtin("let")
-def scheme_let(expr_rest, env):
-    let_frame = Frame(env)
+def scheme_let(expr_rest, env, _=None):
+    if _:
+        let_frame = env
+    else:
+        let_frame = Frame(env)
     define_list = expr_rest.first
     while define_list != nil:
         pair = define_list.first
         let_frame.define(pair.first, scheme_eval(pair.rest.first, env))
         if pair.rest.rest != nil:
-            raise SchemeError("There should be one value to asign in let form!")
+            raise SchemeError(
+                "There should be one value to asign in let form!")
         define_list = define_list.rest
 
     to_eval = expr_rest.rest
     while to_eval != nil:
-        val = scheme_eval(to_eval.first, let_frame)
+        val = scheme_eval(to_eval.first, let_frame, _=_)
         to_eval = to_eval.rest
     return val
 
 
 @other_builtin("mu")
-def scheme_mu(expr_rest, env):
+def scheme_mu(expr_rest, env, _=None):
     formals = expr_rest.first
     body = expr_rest.rest
     if body == nil:
@@ -186,13 +212,17 @@ def scheme_eval(expr, env, _=None):  # Optional third argument is ignored
             raise SchemeError("No valid operator!")
 
     if operator_name in other_builtins:
-        return other_builtins[operator_name](expr.rest, env)
+        return other_builtins[operator_name](expr.rest, env, _=_)
 
     prcd = env.find(operator_name)
     if _ and prcd == _:
-        return Unevaluated(expr, env=env)
+        return Unevaluated(expr.rest, env)
     if prcd:
         if isinstance(prcd, Procedure):
+            if _ and type(prcd) == LambdaProcedure:
+                define_args(prcd, expr.rest, env, deepcopy(env))
+                tail = non_tail_eval(prcd, env)
+                return scheme_eval(tail.first, env, _=_)
             return scheme_apply(prcd, expr.rest, env)
 
     raise SchemeError("Undefined operator " + str(operator_name) + "!")
@@ -255,19 +285,75 @@ def non_tail_eval(procedure, func_frame):
     return temp
 
 
+def is_tail_call(procedure, tail, env:Frame) -> bool:
+    """Check if `tail` is a tail call."""
+    if type(tail) != Pair:
+        return False
+
+    if type(tail.first) == str and env.find(tail.first) == procedure:
+        return True
+
+    if type(tail.first) == Pair and tail.rest == nil:
+        return is_tail_call(procedure, tail.first, env)
+
+    if tail.first == 'if':
+        return is_tail_call(procedure, tail.rest.rest.first, env) or \
+            is_tail_call(procedure, tail.rest.rest.rest.first, env)
+
+    if tail.first == 'cond':
+        tail = tail.rest
+        non_predict = False
+        recursive = False
+        global_frame = create_global_frame()
+        while tail != nil:
+            try:
+                val = scheme_eval(tail.first.first, Frame(global_frame))
+            except SchemeError:
+                non_predict = True
+            if is_tail_call(procedure, tail.first.rest, env):
+                recursive = True
+            if non_predict and recursive:
+                return True
+            tail = tail.rest
+        return False
+
+    tails = ['and', 'or', 'begin', 'let']
+    if tail.first in tails:
+        eval = False
+        if tail.first == 'begin':
+            env = Frame(env)
+            eval = True
+        while tail.rest != nil:
+            tail = tail.rest
+            if eval:
+                scheme_eval(tail.first, env, _=procedure) 
+        return is_tail_call(procedure, tail, env)
+
+    prcd = env.find(tail.first)
+    if prcd and type(prcd) == LambdaProcedure:
+        body = prcd.body
+        while body.rest != nil:
+            body = body.rest
+        return is_tail_call(procedure, body.first, env)
+    
+    return False
+
+
+    
 def tail_eval(procedure, tail, func_frame: Frame):
     """Implement tail recursion without create new frame."""
-    return scheme_eval(tail, func_frame, _=procedure)
-
+    if is_tail_call(procedure, tail, func_frame):
+        unevaluated = scheme_eval(tail, func_frame, _=procedure)
+        while type(unevaluated) == Unevaluated:
+            define_args(procedure, unevaluated.args, unevaluated.env, deepcopy(unevaluated.env))
+            unevaluated = scheme_eval(tail, unevaluated.env, _=procedure)
+        return unevaluated
+    return scheme_eval(tail, func_frame)
 
 class Unevaluated:
-    """Unevaluated expresions in `tail_eval`."""
-
-    def __init__(self, expr, env):
-        self.expr = expr
+    def __init__(self, args, env):
+        self.args = args
         self.env = env
-
-
 # END Problem EC 1
 
 
--- scheme_stubbed/test.py
+++ scheme_stubbed/test.py
@@ -1,13 +1,23 @@
 from scheme import *
 
-def all_eval(exprs):
-    env = create_global_frame()
+def all_eval(exprs, env = create_global_frame()):
     for expr in exprs:
         print(scheme_eval(read_line(expr), env))
 
-all_eval(['(+ 1 1)'])
+f = create_global_frame()
+all_eval(["""(define (sum n total)
+   (begin
+      (define add (lambda (x+1 y) (sum (- x+1 1) y)))
+      (or (and (zero? n) total)
+          (add n (+ n total)))))""", "(sum 1001 0)"],f)
+
+
+#all_eval(["""(define (sum n total)
+#   (cond ((zero? n) total)
+#         ((zero? 0) (sum (- n 1) (+ n total)))
+#         (else 42)))""", "(sum 1001 0)"])
+""""
 #all_eval(['(define square (lambda (x) (* x x)))', '(define (sum-of-squares x y) (+ (square x) (square y)))', '(sum-of-squares 3 4)'])
-"""
 print(scheme_eval(read_line('(f 1000)'), f))
 print(type(scheme_eval(read_line('(f 1000)'), f)))
 """
